aae501fd819ed474ee3f644e61acfeeeb178aef0
ajiu9 <ajiurenern@163.com>
2018-02-01 18:27:10 +0800

    添加music-list组件模板

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script type="text/ecmascript-6">
- export default {}
+export default {}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,14 +1,16 @@
 <template>
 <transition name="slide">
-  <div class="singer-detail"></div>
+  <music-list></music-list>
 </transition>
 </template>
 
 <script type="text/ecmascript-6">
+import MusicList from 'components/music-list/music-list'
 import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import {createSong} from 'common/js/song'
+
 export default {
   data() {
     return {
@@ -47,21 +49,15 @@ export default {
       })
       return ret
     }
+  },
+  components: {
+    MusicList
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import "~common/stylus/variable"
-
-.singer-detail
-  position: fixed
-  z-index: 100
-  left: 0
-  top: 0
-  right: 0
-  bottom: 0
-  background-color: $color-background
 
 .slide-enter-active, .slide-leave-active
   transition: all 0.3s
